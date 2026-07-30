8ecda9c21d144a195d384295bed9daac89115aff
MD ALAM <alkaserphone@MacBook-Air.local>
2026-07-30 11:32:42 +0545

    es6 package by nodeje init

--- 01_Basic/main.mjs
+++ 01_Basic/main.mjs
@@ -5,4 +5,4 @@ import { binarySearch } from './searching.mjs';
 
 console.log(linearSearch([1, 2, 3, 4, 5], 3)); // Output: 2
 
-console.log(binarySearch([1, 2, 3, 4, 5], 4)); // Output: 3
+console.log(binarySearch([1, 2, 3, 4, 5], 4)); // Output: 3 
