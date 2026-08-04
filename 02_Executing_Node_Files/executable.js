5cb7b760e5f33a7c62f08f285f826b29c81bfb91
MD ALAM <alkaserphone@MacBook-Air.local>
2026-08-04 06:31:10 +0545

    add executable Node.js file for direct execution

---
+++ 02_Executing_Node_Files/executable.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+// ========================================
+// DIRECTLY EXECUTING A NODE.JS FILE
+// ========================================
+
+// The first line is called a SHEBANG:
+//
+// #!/usr/bin/env node
+//
+// It tells macOS/Linux:
+// "Use Node.js to execute this file."
+
+
+console.log("🚀 Node.js program started!");
+
+const name = "Masoom";
+
+console.log(`Hello ${name}!`);
+
+console.log("This JavaScript file was executed using ./executable.js");
